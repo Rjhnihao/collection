13fc1ff249394146719b461423ad5e02118549c4
nihao_lx <1492110355@qq.com>
2024-03-22 20:50:48 +0800

    返回类型修改成功！

--- api.go
+++ api.go
@@ -6,115 +6,187 @@ import (
 	"net/http"
 )
 
+type Respone struct {
+	Msg   string `json:"msg"`
+	Result	Result `json:"result"`
+}
 
+type Result struct {
+	Payload interface{} `json:"payload"`
+	Txid	string `json:"txid"`
+	Error	string `json:"error"`
+}
+
+var respone Respone
 func saveCollectionhander(c *gin.Context) {
+
+	respone.Msg="藏品保存失败"
+	respone.Result.Payload=""
 	if err := c.ShouldBindJSON(&collection); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusBadRequest, gin.H{"respone":respone})
 		return
 	}
-	txid, err := serviceSetup.Save(collection)
+	payload,txid, err := serviceSetup.Save(collection)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"msg":"藏品信息保存成功！","txid": txid})
+	respone.Msg="藏品保存成功！"
+	respone.Result.Payload=payload
+	respone.Result.Txid=txid
+	respone.Result.Error=""
+
+	c.JSON(http.StatusOK, gin.H{"respone":respone})
 }
 
 func delCollectionhander(c *gin.Context) {
 
+	respone.Msg="藏品删除失败"
+	respone.Result.Payload=""
 	if err := c.ShouldBindJSON(&collection); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusBadRequest, gin.H{"respone":respone})
 		return
 	}
 
-	txid, err := serviceSetup.Del(collection.Id)
+	payload,txid, err := serviceSetup.Del(collection.Id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"msg":"藏品信息删除成功！","txid": txid})
+	respone.Msg="藏品删除成功！"
+	respone.Result.Payload=payload
+	respone.Result.Txid=txid
+	respone.Result.Error=""
+
+	c.JSON(http.StatusOK, gin.H{"respone":respone})
 }
 
 func updateCollectionhander(c *gin.Context) {
+
+	respone.Msg="藏品更新失败"
+	respone.Result.Payload=""
 	if err := c.ShouldBindJSON(&collection); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusBadRequest,  gin.H{"respone":respone})
 		return
 	}
 
-	txid, err := serviceSetup.Update(collection)
+	payload,txid, err := serviceSetup.Update(collection)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError,  gin.H{"respone":respone})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"msg":"藏品信息更新成功","txid": txid})
+	respone.Msg="藏品更新成功！"
+	respone.Result.Payload=payload
+	respone.Result.Txid=txid
+	respone.Result.Error=""
+
+	c.JSON(http.StatusOK, gin.H{"respone":respone})
 }
 
 func queryCollectionByIdhander(c *gin.Context) {
 
+	respone.Msg="藏品查询失败"
+	respone.Result.Payload=""
 	if err := c.ShouldBindJSON(&collection); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusBadRequest, gin.H{"respone":respone})
 		return
 	}
 
-	result, err := serviceSetup.QueryById(collection.Id)
+	payload,txid, err := serviceSetup.QueryById(collection.Id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
 	var msg interface{}
-	err =json.Unmarshal([]byte(result),&msg)
+	err =json.Unmarshal([]byte(payload),&msg)
 	if err!=nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
+	respone.Msg="藏品查询成功！"
+	respone.Result.Payload=msg
+	respone.Result.Txid=txid
+	respone.Result.Error=""
 
-	c.JSON(http.StatusOK, gin.H{"msg":"查询成功","result": msg})
+	c.JSON(http.StatusOK, gin.H{"respone":respone})
 }
 
 func queryCollectionByHashhander(c *gin.Context) {
+
+	respone.Msg="藏品查询失败"
+	respone.Result.Payload=""
+
 	if err := c.ShouldBindJSON(&collection); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusBadRequest, gin.H{"respone":respone})
 		return
 	}
 
-	result, err := serviceSetup.QueryByHash(collection.CollectionHash)
+	payload,txid, err := serviceSetup.QueryByHash(collection.CollectionHash)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
 	var msg interface{}
-	err =json.Unmarshal([]byte(result),&msg)
+	err =json.Unmarshal([]byte(payload),&msg)
 	if err!=nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"msg":"查询成功","result": msg})
+	respone.Msg="藏品查询成功！"
+	respone.Result.Payload=msg
+	respone.Result.Txid=txid
+	respone.Result.Error=""
+	c.JSON(http.StatusOK, gin.H{"respone":respone})
 }
 
 func queryCollectionByOwnerhander(c *gin.Context) {
 
+	respone.Msg="藏品查询失败"
+	respone.Result.Payload=""
+
 	if err := c.ShouldBindJSON(&collection); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusBadRequest, gin.H{"respone":respone})
 		return
 	}
-	result, err := serviceSetup.QueryByOwner(collection.Owner)
+	payload,txid, err := serviceSetup.QueryByOwner(collection.Owner)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 	var msg interface{}
-	err =json.Unmarshal([]byte(result),&msg)
+	err =json.Unmarshal([]byte(payload),&msg)
 	if err!=nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		respone.Result.Error=err.Error()
+		c.JSON(http.StatusInternalServerError, gin.H{"respone":respone})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"msg":"查询成功","result": msg})
+	respone.Msg="藏品查询成功！"
+	respone.Result.Payload=msg
+	respone.Result.Txid=txid
+	respone.Result.Error=""
+
+	c.JSON(http.StatusOK, gin.H{"respone":respone})
 }
--- service/nftService.go
+++ service/nftService.go
@@ -8,7 +8,7 @@ import (
 
 //(以下是通过key值进行操作)
 //调用链码增加藏品信息
-func (t *ServiceSetup) Save(collection Collection) ( string, error) {
+func (t *ServiceSetup) Save(collection Collection) ( string,string, error) {
 
 	eventID := "SaveCollection"
 	reg, notifier := regitserEvent(t.Client, t.ChaincodeID, eventID)
@@ -16,119 +16,124 @@ func (t *ServiceSetup) Save(collection Collection) ( string, error) {
 
 	b, err := json.Marshal(collection)
 	if err != nil {
-		return  "", fmt.Errorf("指定的collection对象序列化时发生错误")
+		return  "","", fmt.Errorf("指定的collection对象序列化时发生错误")
 	}
 
-	fmt.Println("开始执行链码(调用链码增加藏品信息)")
+
 	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "saveCollection", Args: [][]byte{b, []byte(eventID)}}
-	respone, err := t.Client.Execute(req)
+	response, err := t.Client.Execute(req)
 	if err != nil {
-		return "", err
+		return "","", err
 	}
 
 	err = eventResult(notifier, eventID)
 	if err != nil {
-		return "", err
+		return "","", err
 	}
+	payload:=response.Payload
+	txid := response.TransactionID
 
-	txid :=respone.TransactionID
-	return	string(txid),nil
+	return	string(payload),string(txid),nil
 }
 
 //调用链码删除藏品信息
-func (t *ServiceSetup) Del(Id string) (string, error) {
+func (t *ServiceSetup) Del(Id string) (string,string, error) {
 
 	eventID := "DelCollection"
 	reg, notifier := regitserEvent(t.Client, t.ChaincodeID, eventID)
 	defer t.Client.UnregisterChaincodeEvent(reg)
-	fmt.Println("开始执行链码(调用链码删除藏品信息)")
+
 
 	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "delCollection", Args: [][]byte{[]byte(Id), []byte(eventID)}}
-	respone, err := t.Client.Execute(req)
+	response, err := t.Client.Execute(req)
 	if err != nil {
-		return "", err
+		return "","", err
 	}
 
 	err = eventResult(notifier, eventID)
 	if err != nil {
-		return "", err
+		return "","", err
 	}
 
-	txid :=respone.TransactionID
-	return	string(txid),nil
+	payload:=response.Payload
+	txid := response.TransactionID
+
+	return	string(payload),string(txid),nil
 }
 
 //调用链码更新藏品信息
-func (t *ServiceSetup) Update(collection Collection) (string, error) {
+func (t *ServiceSetup) Update(collection Collection) (string,string, error) {
 
 	eventID := "UpdateCollection"
 	reg, notifier := regitserEvent(t.Client, t.ChaincodeID, eventID)
 	defer t.Client.UnregisterChaincodeEvent(reg)
 
 
-	fmt.Println("开始执行链码(调用链码更新藏品信息)")
+
 	b, err := json.Marshal(collection)
 	if err != nil {
-		return "", fmt.Errorf("指定的collection对象序列化时发生错误")
+		return "","", fmt.Errorf("指定的collection对象序列化时发生错误")
 	}
 
 	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "updateCollection", Args: [][]byte{b, []byte(eventID)}}
-	respone, err := t.Client.Execute(req)
+	response, err := t.Client.Execute(req)
 	if err != nil {
-		return "", err
+		return "","", err
 	}
 
 	err = eventResult(notifier, eventID)
 	if err != nil {
-		return "", err
+		return "","", err
 	}
 
-	txid :=respone.TransactionID
+	payload:=response.Payload
+	txid := response.TransactionID
 
-	return	string(txid),nil
+	return	string(payload),string(txid),nil
 }
 
 //调用链码通过藏品id查询数据
-func (t *ServiceSetup) QueryById(Id string) (string, error){
-	fmt.Println("开始执行链码(调用链码通过藏品id查询数据)")
+func (t *ServiceSetup) QueryById(Id string) (string,string, error){
+
 	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "queryCollectionById", Args: [][]byte{[]byte(Id)}}
-	respone, err := t.Client.Query(req)
+	response, err := t.Client.Query(req)
 	if err != nil {
-		return "[]byte{0x00}", err
+		return "","", err
 	}
 
-	Payload :=respone.Payload
+	payload:=response.Payload
+	txid := response.TransactionID
 
-	return	string(Payload),nil
+	return	string(payload),string(txid),nil
 }
 
 //(以下是通过富查询)
 //调用链码通过Hash查询数据
-func (t *ServiceSetup) QueryByHash(CollectionHash string) (string, error){
+func (t *ServiceSetup) QueryByHash(CollectionHash string) (string, string, error){
 
-	fmt.Println("开始执行链码(调用链码通过Hash查询数据)")
 	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "queryCollectionByHash", Args: [][]byte{[]byte(CollectionHash)}}
-	respone, err := t.Client.Query(req)
+	response, err := t.Client.Query(req)
 	if err != nil {
-		return "[]byte{0x00}", err
+		return "","", err
 	}
 
-	Payload :=respone.Payload
+	payload:=response.Payload
+	txid := response.TransactionID
 
-	return	string(Payload),nil
+	return	string(payload),string(txid),nil
 }
 
 //调用链码通过拥有者查信息
-func (t *ServiceSetup) QueryByOwner(Owner string) (string, error){
+func (t *ServiceSetup) QueryByOwner(Owner string) (string, string, error){
 
-	fmt.Println("开始执行链码(调用链码通过拥有者查询数据)")
 	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "queryCollectionByOwner", Args: [][]byte{[]byte(Owner)}}
-	respone, err := t.Client.Query(req)
+	response, err := t.Client.Query(req)
 	if err != nil {
-		return "[]byte{0x00}", err
+		return "","", err
 	}
 
-	Payload :=respone.Payload
+	payload:=response.Payload
+	txid := response.TransactionID
 
-	return	string(Payload),nil
+	return	string(payload),string(txid),nil
 }
